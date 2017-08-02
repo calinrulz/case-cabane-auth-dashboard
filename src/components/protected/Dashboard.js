6a3653d63a53ba4b8147a566c02d2b4f444412a3
Calin Georgescu <calgeorg@cisco.com>
2017-08-02 14:29:51 +0300

    Added react-firebase-upload and styled the component

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import FileUploader from 'react-firebase-file-uploader';
 
 export default class Dashboard extends Component {
-  // constructor() {
-  //   // Get Elements
-  //   let uploader = document.getElementById('upload');
-  //   let fileButton = document.getElementById('fileButton');
+  state = {
+    avatar: '',
+    isUploading: false,
+    progress: 0,
+    avatarURL: ''
+  };
 
-  //   // Listen for file selection
-  //   fileButton.addEventListener('change', (e) => {
-  //     // Get File
-
-
-  //     // Create a Storage ref
-
-
-  //     // Upload File
-
-
-  //     // Update Progress Bar
-
-
-  //     super();
-  //   });
-  // }
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = (progress) => this.setState({ progress });
+  handleUploadError = (error) => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  }
+  handleUploadSuccess = (filename) => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase.storage().ref('images').child(filename).getDownloadURL().then(
+      url => this.setState({ avatarURL: url })
+    );
+  };
 
   render () {
     return (
@@ -34,12 +33,37 @@ export default class Dashboard extends Component {
         <div className="row">
           <div className="col-md-6 col-md-offset-3 upload-container">
             <div className="progress">
-              <div id="uploader" className="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                <span className="sr-only">0% Complete</span>
+              <div className="progress-bar progress-bar-success progress-bar-striped"
+                   role="progressbar"
+                   aria-valuenow={ this.state.progress }
+                   aria-valuemin="0"
+                   aria-valuemax="100"
+                   style={{ width: `${this.state.progress}%` }}
+              >
+                {this.state.isUploading &&
+                  <span>{ this.state.progress }% Complete</span>
+                }
               </div>
             </div>
 
-            <input type="file" defaultValue="upload" id="fileButton" />
+            <div className="chose-file-container">
+              <FileUploader
+                accept="image/*"
+                name="avatar"
+                randomizeFilename
+                storageRef={ firebase.storage().ref('images') }
+                onUploadStart={ this.handleUploadStart }
+                onUploadError={ this.handleUploadError }
+                onUploadSuccess={ this.handleUploadSuccess }
+                onProgress={ this.handleProgress }
+              />
+            </div>
+          </div>
+
+          <div className="col-md-6 col-md-offset-3">
+            {this.state.avatarURL &&
+              <img src={ this.state.avatarURL } alt="" className="uploaded-image" />
+            }
           </div>
         </div>
       </div>
